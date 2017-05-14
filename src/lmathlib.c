31c9be65104f96cc53f1321481409e0a08afe8a9
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2017-05-14 18:21:26 -0400

    Break math.random prediction on modern x86 CPUs

--- src/lmathlib.c
+++ src/lmathlib.c
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include <math.h>
+#include <immintrin.h>
 
 #define lmathlib_c
 #define LUA_LIB
@@ -181,7 +182,9 @@ static int math_max (lua_State *L) {
 static int math_random (lua_State *L) {
   /* the `%' avoids the (rare) case of r==1, and is needed also because on
      some systems (SunOS!) `rand()' may return a value larger than RAND_MAX */
-  lua_Number r = (lua_Number)(rand()%RAND_MAX) / (lua_Number)RAND_MAX;
+  unsigned int num;
+  __builtin_ia32_rdrand32_step(&num);
+  lua_Number r = (lua_Number)(num % RAND_MAX) / (lua_Number)RAND_MAX;
   switch (lua_gettop(L)) {  /* check number of arguments */
     case 0: {  /* no arguments */
       lua_pushnumber(L, r);  /* Number between 0 and 1 */
@@ -260,4 +263,3 @@ LUALIB_API int luaopen_math (lua_State *L) {
 #endif
   return 1;
 }
-
